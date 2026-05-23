3c3a22d62872ebe2ecbdaa1b446227773c2975b2
Aryan Thakur <aryan197297@gmail.com>
2026-05-23 17:09:23 +0530

    feat(setup): portfolio monorepo app

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,7 @@
 # inside scripts/setup.sh
 git config core.hooksPath .githooks
 echo "✅ Git hooks configured"
+
+# Install node, nvm, project supported node version
+nvm install v24.16.0 && nvm use v24.16.0
+echo "✅ Node.js v24.16.0 installed and set as default"

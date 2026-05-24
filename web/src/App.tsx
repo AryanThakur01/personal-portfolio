5fad2fdf319463c426c9bfa11a0001ca8fec3870
Aryan Thakur <aryan197297@gmail.com>
2026-05-25 00:09:21 +0530

    feat(app): mount navbar in app root

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,12 @@
+import { Navbar } from "./components/Navbar";
+
 function App() {
-  return <></>;
+  return (
+    <div className="min-h-screen bg-bg text-text font-sans">
+      <Navbar />
+      <main className="pt-12" />
+    </div>
+  );
 }
 
 export default App;

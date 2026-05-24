630c92e7475cb785a50483dfdce465a753786edf
Aryan Thakur <aryan197297@gmail.com>
2026-05-25 00:25:08 +0530

    feat(app): mount hero section

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,13 @@
 import { Navbar } from "./components/Navbar";
+import { Hero } from "./components/Hero";
 
 function App() {
   return (
     <div className="min-h-screen bg-bg text-text font-sans">
       <Navbar />
-      <main className="pt-12" />
+      <main>
+        <Hero />
+      </main>
     </div>
   );
 }

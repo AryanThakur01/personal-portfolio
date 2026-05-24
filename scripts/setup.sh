9d95d7ceca92400f7418c330c9e60ec804c9f8ce
Aryan Thakur <aryan197297@gmail.com>
2026-05-24 23:51:10 +0530

    update: setup script to install setup of vite project

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,3 +5,7 @@ echo "✅ Git hooks configured"
 # Install node, nvm, project supported node version
 nvm install v24.16.0 && nvm use v24.16.0
 echo "✅ Node.js v24.16.0 installed and set as default"
+
+# Setup and start the project
+cd ./web && pnpm install
+echo "✅ Web dependencies installed"
